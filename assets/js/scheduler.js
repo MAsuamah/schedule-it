a453bee4aba0b15cfcc8143025a5267e2aaf8ca5
Michelle <michey.asmah21@gmail.com>
2021-03-26 12:25:43 -0400

    array for timeblocks

--- assets/js/scheduler.js
+++ assets/js/scheduler.js
@@ -3,11 +3,49 @@
 var currentDate = moment()
 console.log(currentDate)
 
-
 //Display date in the jumbotron
-var showDate = moment(currentDate).format('MMMM Do YYYY');
+var showDate = moment(currentDate).format('dddd, MMMM Do YYYY');
 
 $('#currentDay').append(showDate)
 
+//Array that will be used to display Timeblocks
 
+var workDay = [
+  {
+    hour: '9am',
+    event: ''
+  },
+  {
+    hour: '10am',
+    event: '',
+  },
+  {
+    hour: '11am',
+    event: '',
+  },
+  {
+    hour: '12pm',
+    event: '',
+  },
+  {
+    hour: '1pm',
+    event: '',
+  },
+  {
+    hour: '2pm',
+    event: '',
+  },
+  {
+    hour: '3pm',
+    event: '',
+  },
+  {
+    hour: '4pm',
+    event: '',
+  },
+  {
+    hour: '5pm',
+    event: '',
+  }  
+]
 
